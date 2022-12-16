484960e995a02d25397af258ba2b42a26748698b
yan candaes <candaes.yan@gmail.com>
2022-12-16 20:45:28 +0100

    addInterventionActivity timepicker scroll comment

--- app/src/main/java/yan/candaes/gmagro/ui/addInterventionActivity.java
+++ app/src/main/java/yan/candaes/gmagro/ui/addInterventionActivity.java
@@ -1,48 +1,52 @@
 package yan.candaes.gmagro.ui;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import yan.candaes.gmagro.R;
 import yan.candaes.gmagro.dao.DaoIntervention;
+import yan.candaes.gmagro.dao.DaoUtilisateur;
 import yan.candaes.gmagro.dao.Delegate;
+import yan.candaes.gmagro.tools.Tools;
 
 public class addInterventionActivity extends AppCompatActivity {
+    static Boolean isInnit = false;
+    ArrayAdapter adaActivite;
+    ArrayAdapter adaSO;
+    ArrayAdapter adaSD;
+    ArrayAdapter adaCO;
+    ArrayAdapter adaCD;
+    ArrayAdapter adaMachine;
+    TextView heureD;
+    TextView heureF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_intervention);
 
-        DaoIntervention.getInstance().getAscod(new Delegate() {
-            @Override
-            public void WSRequestIsDone(Object result) {
+        innit();
+
+
+        heureD = ((TextView) findViewById(R.id.addInterBtnHeureDebut));
+        heureD.setOnClickListener(Tools.TimePicker(addInterventionActivity.this, heureD));
+        heureF = ((TextView) findViewById(R.id.addInterBtnHeureFin));
+        heureF.setOnClickListener(Tools.TimePicker(addInterventionActivity.this, heureF));
+        CheckBox isTerminee = ((CheckBox) findViewById(R.id.addInterCbInterventionTerminee));
+        isTerminee.setOnClickListener(v -> {
+            if (isTerminee.isChecked()) {
+                    heureF.setVisibility(View.VISIBLE);
+                } else {
+                    heureF.setVisibility(View.INVISIBLE);
+                }
             }
-        });
-        DaoIntervention.getInstance().getMachines(new Delegate() {
-            @Override
-            public void WSRequestIsDone(Object result) {
-            }
-        });
-
-            ArrayAdapter adaActivite = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesActivites());
-            ArrayAdapter adaSO = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesSO());
-            ArrayAdapter adaSD = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesSD());
-            ArrayAdapter adaCO = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesCO());
-            ArrayAdapter adaCD = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesCD());
-            ArrayAdapter adaMachine = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesMachines());
-            ((Spinner) findViewById(R.id.addInterSpinActivite)).setAdapter(adaActivite);
-            ((Spinner) findViewById(R.id.addInterSpinSO)).setAdapter(adaSO);
-            ((Spinner) findViewById(R.id.addInterSpinSD)).setAdapter(adaSD);
-            ((Spinner) findViewById(R.id.addInterSpinCO)).setAdapter(adaCO);
-            ((Spinner) findViewById(R.id.addInterSpinCD)).setAdapter(adaCD);
-            ((Spinner) findViewById(R.id.addInterSpinMachine)).setAdapter(adaMachine);
-
-
-
+        );
 
 
 
@@ -59,6 +63,44 @@ public class addInterventionActivity extends AppCompatActivity {
             });
         }*/
 
+
+    }
+
+
+    private void innit() {
+        if (!isInnit) {
+            DaoIntervention.getInstance().getAscod(new Delegate() {
+                @Override
+                public void WSRequestIsDone(Object result) {
+                    adaActivite.notifyDataSetChanged();
+                    adaCD.notifyDataSetChanged();
+                    adaCO.notifyDataSetChanged();
+                    adaSD.notifyDataSetChanged();
+                    adaSO.notifyDataSetChanged();
+                }
+            });
+            DaoIntervention.getInstance().getMachines(new Delegate() {
+                @Override
+                public void WSRequestIsDone(Object result) {
+                    adaMachine.notifyDataSetChanged();
+                }
+            });
+        }
+        adaActivite = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesActivites());
+        adaSO = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesSO());
+        adaSD = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesSD());
+        adaCO = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesCO());
+        adaCD = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesCD());
+        adaMachine = new ArrayAdapter(this, android.R.layout.simple_list_item_1, DaoIntervention.getInstance().getLesMachines());
+        ((Spinner) findViewById(R.id.addInterSpinActivite)).setAdapter(adaActivite);
+        ((Spinner) findViewById(R.id.addInterSpinSO)).setAdapter(adaSO);
+        ((Spinner) findViewById(R.id.addInterSpinSD)).setAdapter(adaSD);
+        ((Spinner) findViewById(R.id.addInterSpinCO)).setAdapter(adaCO);
+        ((Spinner) findViewById(R.id.addInterSpinCD)).setAdapter(adaCD);
+        ((Spinner) findViewById(R.id.addInterSpinMachine)).setAdapter(adaMachine);
+        ((TextView) findViewById(R.id.addInterTextViewCo)).setText(((TextView) findViewById(R.id.addInterTextViewCo)).getText() + " " + DaoUtilisateur.getInstance().getLogUtilisateur());
+
+        isInnit = true;
 
     }
 }
